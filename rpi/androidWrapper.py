5457feb6d045bed78da338f6ac19ea382245a617
huifung7 <huifung_yap@outlook.com>
2014-09-29 10:03:26 +0800

    wrapper

--- rpi/androidWrapper.py
+++ rpi/androidWrapper.py
@@ -1,30 +1,46 @@
+import time
 from bluetooth import *
 
- class androidWrapper:
+class androidWrapper:
 	def __init__(self):
-		uuid="00001101-0000-1000-8000-00805F9B34FB"
-	def connect(self):
-		server_sock= BluetoothSocket(RFCOMM)
-		server_sock.bind(("",PORT_ANY))
-		server_sock.listen(1)
-		port= server_sock.getsockname()[1]
-		advertise_service( server_sock, "MDPGrp18",
-				   service_id= uuid,
-				   service_classes= [uuid, 
-SERIAL_PORT_CLASS],
+		self.uuid="00001101-0000-1000-8000-00805F9B34FB"
+		self.server_sock= BluetoothSocket(RFCOMM)
+		self.server_sock.bind(("",PORT_ANY))
+		self.client_sock = None
+		self.client_info = None
+		self.server_sock.listen(1)
+		self.port = self.server_sock.getsockname()[1]
+		advertise_service( self.server_sock, "MDPGrp18",
+				   service_id= self.uuid,
+				   service_classes= [self.uuid, SERIAL_PORT_CLASS],
 				   profiles= [SERIAL_PORT_PROFILE],
 				  )
-		print "waiting for connection on RFCOMM channel %d" % 
-(port)
-		client_sock, client_info= server_sock.accept()
-		print "Accepted connection from ", client_info
-	def disconnect(self):
-		client_sock.close()
-		server_sock.close()
+
+	def startBTService(threadName, self, delay, finish1):
+		while True:
+			time.sleep(delay)
+			if self.client_sock is not None:
+				finish1= True
+				print "Accepted connection from ", self.client_info
+				break
+			print "waiting for connection on RFCOMM channel %d" % (self.port)
+			self.client_sock, self.client_info = self.server_sock.accept()
+
+
+	def stopBTService(self):
+		self.client_sock.close()
+		self.client_info = None
+		self.server_sock.close()
+
 	def write(self,msg):
 		self.client_sock.send(msg)
 		print "Write to Android: %s" %(msg)
+
 	def read(self):
-		msg = self.client_sock.recv(1024)
-		print "Read from Android: %s" %(msg)
-		return msg
+		if self.client_sock is not None:
+			msg = self.client_sock.recv(1024)
+			print "Read from Android: %s" %(msg)
+			return msg
+		else:
+			return None
+
--- rpi/pcWrapper.py
+++ rpi/pcWrapper.py
@@ -1,22 +1,31 @@
-import socket class pcWrapper:
+import socket
+import time
+
+class pcWrapper:
 	def __init__(self):
 		self.tcp_ip = "192.168.18.1"
 		self.port = 5143
-	
-	def connect(self):
-		self.ipSocket = socket.socket(socket.AF_INET, 
-socket.SOCK_DGRAM)
-		
-		self.ipSocket.setsockopt(socket.SOL_SOCKET, 
-socket.SO_REUSEADDR,1)
-		self.ipSocket.setsockopt(socket.SOL_SOCKET, 
-socket.SO_BROADCAST,1)
+		self.ipSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.ipSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+		self.ipSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
 		self.ipSocket.bind((self.tcp_ip, self.port))
-		print "waiting..."
-		self.pcaddr = self.ipSocket.recvfrom(1024)[1]
-		print "wifi link up"
-	def disconnect(self):
+		self.pcaddr = None
+
+	def startIPService(threadName, self, delay, finish2):
+		while True:
+			time.sleep(delay)
+			if self.pcaddr is not None:
+				finish2=True
+				print "wifi link up"
+				break
+			print "waiting for WIFI connection..."
+			self.pcaddr = self.ipSocket.recvfrom(1024)[1]
+
+
+
+	def stopIPService(self):
 		self.ipSocket.close()
+
 	def write(self, msg):
 		self.ipSocket.sendto(msg, self.pcaddr)
 		print "Write to PC: %s" %(msg)
@@ -25,9 +34,16 @@ socket.SO_BROADCAST,1)
 			msg = self.ipSocket.recvfrom(1024)[0]
 			print "Read from PC: %s" % (msg)
 			return msg
-#test= pcWrapper() test.connect() while True:
-#	input=int(raw_input("enter 0 to read or 1 to write:")) if 
-#	input==0:
-#		test.read() elif input==1: test.write(raw_input("enter 
-#	msg:")) else:
+
+#test= pcWrapper()
+#test.connect()
+#while True:
+#	input=int(raw_input("enter 0 to read or 1 to write:"))
+#	if input==0:
+#		test.read()
+#	elif input==1:
+#		test.write(raw_input("enter msg:"))
+#	else:
 #		break
+
+
