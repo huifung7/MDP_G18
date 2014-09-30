920170a309715dca3f7520b5255b71950c37eb4f
huifung7 <huifung_yap@outlook.com>
2014-09-30 11:34:38 +0800

    indentation made in arduinoWrapper

--- rpi/arduinoWrapper.py
+++ rpi/arduinoWrapper.py
@@ -3,29 +3,29 @@ import time
 
 class arduinoWrapper:
 
-    def __init__(self):
-        self.port = '/dev/ttyACM0'
-        self.baud = 115200
-
+	def __init__(self):
+	self.port = '/dev/ttyACM0'
+		self.baud = 115200
+		
     def startSerialService(self, delay, ready3):
 		print "Waiting for Serial Connection..."
 		self.serSock = serial.Serial (self.port, self.baud)
-        #init socket connection
+		#init socket connection
 		#self.serSock.write("")
 		#self.serSock.write("")
 		print "Serial Connection Link Up..."
 		ready3[0]=True
 		
-def stopSerialService(self):
-	self.serSock.close()
+	def stopSerialService(self):
+		self.serSock.close()
 		
-def write(self,msg):
-	if self.serSock.getCTS() == True:
-		self.serSock.write(msg)
-		print "Write to Arduino: %s" %(msg)
-
-    def read(self):
-        msg = self.serSock.readline()
+	def write(self,msg):
+		if self.serSock.getCTS() == True:
+			self.serSock.write(msg)
+			print "Write to Arduino: %s" %(msg)
+			
+	def read(self):
+		msg = self.serSock.readline()
 		print "%s" %(self.serSock.inWaiting())
-        print "Read from Arduino: %s" %(msg)
-        return msg
+		print "Read from Arduino: %s" %(msg)
+		return msg
