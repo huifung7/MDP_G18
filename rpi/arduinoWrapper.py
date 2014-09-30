ff0d93ab54bf88a99813e60ed5916245de5a1720
huifung7 <huifung_yap@outlook.com>
2014-09-30 11:49:00 +0800

    indentation arduinoWrapper

--- rpi/arduinoWrapper.py
+++ rpi/arduinoWrapper.py
@@ -6,8 +6,8 @@ class arduinoWrapper:
 	def __init__(self):
 		self.port = '/dev/ttyACM0'
 		self.baud = 115200
-		
-    def startSerialService(self, delay, ready3):
+
+	def startSerialService(self, delay, ready3):
 		print "Waiting for Serial Connection..."
 		self.serSock = serial.Serial (self.port, self.baud)
 		#init socket connection
@@ -15,15 +15,15 @@ class arduinoWrapper:
 		#self.serSock.write("")
 		print "Serial Connection Link Up..."
 		ready3[0]=True
-		
+
 	def stopSerialService(self):
 		self.serSock.close()
-		
+
 	def write(self,msg):
 		if self.serSock.getCTS() == True:
 			self.serSock.write(msg)
 			print "Write to Arduino: %s" %(msg)
-			
+
 	def read(self):
 		msg = self.serSock.readline()
 		print "%s" %(self.serSock.inWaiting())
