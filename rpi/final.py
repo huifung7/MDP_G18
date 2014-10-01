feb7c119027371df2a7a25d630086a4a819ed13d
huifung7 <huifung_yap@outlook.com>
2014-10-01 15:47:44 +0800

    comment out ser and bt code

--- rpi/final.py
+++ rpi/final.py
@@ -12,47 +12,45 @@ serWrap = arduinoWrapper()
 btWrap = androidWrapper()
 
 pcWrap.startIPService()
-serWrap.startSerialService()
-btWrap.startBTService()
+#serWrap.startSerialService()
+#btWrap.startBTService()
 print "BT, PC and Serial Connection UP..."
-
-#msg = ""
-
-#while(msg!="btstart"):
-#	msg = bt.read()
 
 msg = pcWrap.read()
 
 msg="hey"
 while(msg!="START"):
-	msg = btWrap.read()
+	#msg = btWrap.read()
 	pcWrap.write(msg)
 
 while 1:
 	#pc give command to arduino move
 	msg = pcWrap.read()
 	if (msg == "done/"):
+		print "break out"
 		break;
-	btWrap.write(msg)
-	serWrap.write(msg)
+	#btWrap.write(msg)
+	#serWrap.write(msg)
 
 	#arduino feed the sensor readings
-	msg2 = serWrap.read()
+	#msg2 = serWrap.read()
+	msg2 = "hello"
 	pcWrap.write(msg2)
-	btWrap.write(msg2)
+	#btWrap.write(msg2)
 
 	msg="hey"
 while(msg!="START"):
-	msg = btWrap.read()
+	#msg = btWrap.read()
 	pcWrap.write(msg)
 
 while 1:
 	#pc give command to arduino move
 	msg = pcWrap.read()
-	btWrap.write(msg)
-	serWrap.write(msg)
+	#btWrap.write(msg)
+	#serWrap.write(msg)
 
 	#arduino feed the sensor readings
-	msg2 = serWrap.read()
+	#msg2 = serWrap.read()
+	msg2 = "og"
 	pcWrap.write(msg2)
-	btWrap.write(msg2)
+	#btWrap.write(msg2)
