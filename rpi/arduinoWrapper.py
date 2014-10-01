7f0221da825748265da06e09da93a517a86e4d5a
huifung7 <huifung_yap@outlook.com>
2014-10-01 13:07:41 +0800

    update changes to mdp_g18 repo

--- rpi/arduinoWrapper.py
+++ rpi/arduinoWrapper.py
@@ -1,31 +1,27 @@
 import serial
-import time
 
 class arduinoWrapper:
-
 	def __init__(self):
 		self.port = '/dev/ttyACM0'
-		self.baud = 115200
+		self.baud = 9600
+		self.translation = {'FORWARD':'F', 'LEFT':'L', 'RIGHT':'R', 'STOP':'B'}
 
-	def startSerialService(self, delay, ready3):
-		print "Waiting for Serial Connection..."
-		self.serSock = serial.Serial (self.port, self.baud)
+	def startSerialService(self, ready3):
+		self.serSock = serial.Serial(self.port, self.baud)
 		#init socket connection
-		#self.serSock.write("")
-		#self.serSock.write("")
-		print "Serial Connection Link Up..."
+		self.serSock.write("")
+		self.serSock.write("")
+		print "serial link up"
 		ready3[0]=True
 
 	def stopSerialService(self):
 		self.serSock.close()
 
 	def write(self,msg):
-		if self.serSock.getCTS() == True:
-			self.serSock.write(msg)
-			print "Write to Arduino: %s" %(msg)
+			self.serSock.write(self.translation[msg])
+			print "Received: %s Send to Arduino: %s" %(msg, self.translation[msg])
 
 	def read(self):
 		msg = self.serSock.readline()
-		print "%s" %(self.serSock.inWaiting())
-		print "Read from Arduino: %s" %(msg)
+		#print "Read from Arduino: %s" %(msg)
 		return msg
